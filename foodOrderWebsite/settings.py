4ad4c71ff6d6b7472d404fd0172bec5a7f6f8b11
jaismeneverma <jaismene2397@yahoo.in>
2018-04-18 20:04:03 +0530

    added css

--- foodOrderWebsite/settings.py
+++ foodOrderWebsite/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
+
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'app/templates'),
